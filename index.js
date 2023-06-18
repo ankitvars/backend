79af2f107acc130cfd5c1563d63a62f7f1be4e46
Ankit Varshney <varshneyankit011@gmail.com>
2023-06-18 15:36:37 +0530

    Working on socket

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const connectToMongo = require('./db');
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http');
+const port = 7000;
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.use(express.json());
-app.use("/api", require("./routes/authroute"));
+app.use('/api', require('./routes/authroute'));
 
 app.get('/', (req, res) => {
-  res.send('This is a realtime chat application!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 connectToMongo();
